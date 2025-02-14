31567a60c715cbcc51eccbea3d402e695e8060d7
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2025-02-15 00:28:32 +0530

    frontend

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,22 +1,5 @@
 import "@/styles/globals.css";
-import { WagmiConfig } from "wagmi";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { config } from '../Context/Tracking';
-import { TrackingProvider } from '../Context/Tracking';
-import { Navbar, Footer } from "../Components";
-
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
-  return (
-    <WagmiConfig config={config}>
-      <QueryClientProvider client={queryClient}>
-        <TrackingProvider>
-          <Navbar />
-          <Component {...pageProps} />
-          <Footer />
-        </TrackingProvider>
-      </QueryClientProvider>
-    </WagmiConfig>
-  );
+  return <Component {...pageProps} />;
 }
